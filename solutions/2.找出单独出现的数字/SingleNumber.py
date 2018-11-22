128bc69145d9a6352c4f00d8b381c8fbca0f1283
zhangqi <zhangqi@zhangqideMac-mini.local>
2018-11-22 20:41:39 +0800

    q2

---
+++ solutions/2.找出单独出现的数字/SingleNumber.py
@@ -0,0 +1,19 @@
+# Author : doubleQ
+# Date   : 2018-11-22
+
+def solution(line):
+    # 缩进请使用 4 个空格，遵循 PEP8 规范
+    # 返回处理后的结果
+    nums = [int(i) for i in line.rstrip().split()]
+    cur = nums[0]
+    count = 1
+    for i in nums[1:]:
+        if count == 0:
+            cur = i
+            count = 1
+        else:
+            if cur == i:
+                count += 1
+            else:
+                count -= 1
+    return str(cur)
