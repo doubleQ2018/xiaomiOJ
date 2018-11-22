b6b7cc3d58b1b3f77dc5900b38f761256a61911b
zhangqi <zhangqi@zhangqideMac-mini.local>
2018-11-22 20:10:18 +0800

    test1

---
+++ solutions/1.A+B/add.cpp
@@ -0,0 +1,17 @@
+// Author : Qi Zhang
+// Date   : 2018-11-22
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    while (cin >> a >> b) {
+        cout << a+b << endl;
+    }
+
+    return 0;
+}
